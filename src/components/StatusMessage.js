067b44fefb937628c010383c9a1310969c5c47db
Saikat <paulsaikat8364@gmail.com>
2021-03-11 16:11:11 +0530

    Final Styling

--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -3,9 +3,9 @@ import React from 'react'
 const StatusMessage = ({winner , current}) => {
    
     const noMovesLeft = current.board.every((el)=> el!=null);
-     return <h2>{winner && `Winner is ${winner}`}
-     {!winner && !noMovesLeft &&`The next player is ${current.isXNext? 'X':"O"}`}
-     {!winner && noMovesLeft && `Match Draw!`}</h2>
+     return <div className='status-message'>{winner && <>Winner is <span className={winner==='X'? 'text-green':'text-orange'}>{winner}</span></>}
+     {!winner && !noMovesLeft && <>The next player is <span className={current.isXNext? 'text-green':'text-orange'}>{current.isXNext? 'X':"O"}</span></>}
+     {!winner && noMovesLeft && <><span className="text-green">X </span>and <span className="text-orange">O</span> tied!</>}</div>
   
 }
 
